71a8b746298844d729a0b29b90ffaf7603b0c1ca
UNIVERSE\aillarionov <illarionoff.a@gmail.com>
2023-03-13 21:45:56 +0100

    Initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiCaching.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCaching.Data;
+
+public class AppDbContext : DbContext
+{
+	public DbSet<Driver> Drivers { get; set; }
+	public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+	{
+
+	}
+}
---
+++ Models/Driver.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiCaching.Models;
+
+public class Driver
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int RacingNumber { get; set; }
+    public string? Team { get; set; }
+}
